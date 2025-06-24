5bae99aaf668bf499d499147b955aca69f36900b
Jesse <jesse.yegon@student.moringaschool.com>
2025-06-24 12:39:43 +0300

    new signup

---
+++ controllers/auth_controller.py
@@ -0,0 +1,30 @@
+from flask import request, jsonify
+from models import db, User
+from flask_jwt_extended import create_access_token
+
+def register():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if User.query.filter_by(username=username).first():
+        return jsonify({'message':'User already exists'}), 400
+
+    user = User(username=username)
+    user.set_password(password)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({'message':'User already exists'})
+
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.check_password(password):
+        token = create_access_token(identity=user.id):
+        return jsonify ({'access_token': token})
+    return jsonify({'message': 'invalid details'}), 401    
