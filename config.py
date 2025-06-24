d130b840c6354f6889c829a8985a2155a1ece05a
Jesse <jesse.yegon@student.moringaschool.com>
2025-06-24 14:50:23 +0300

    new config

--- config.py
+++ config.py
@@ -1,2 +1,12 @@
 
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your-jwt-secret-key'
 
